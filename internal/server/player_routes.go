c9f375a144f59e7032497d85d3147faf83bdf525
anjaliityagi <anjalityagi8923@gmail.com>
2026-05-28 20:40:42 +0530

    corrected bugs

---
+++ internal/server/player_routes.go
@@ -0,0 +1,22 @@
+package server
+
+import (
+	"CricketDuniya-Backend/internal/handlers"
+	"CricketDuniya-Backend/internal/middleware"
+
+	"github.com/gin-gonic/gin"
+)
+
+func registerPlayerRoutes(rg *gin.RouterGroup) {
+	rg.GET("/players", handlers.GetPlayersDirectory)
+
+	players := rg.Group("/")
+	players.Use(middleware.AuthMiddleware())
+	{
+		//players.POST("/teams/:id/players", handlers.CreatePlayer)
+		players.DELETE("/players/:id", handlers.DeletePlayer)
+		players.POST("/team-players", handlers.AddPlayersToTeams)
+		players.PUT("players/:player_id/assign-captain", handlers.AssignCaptain)
+		players.PUT("players/:player_id/assign-umpire", handlers.AssignUmpire)
+	}
+}
